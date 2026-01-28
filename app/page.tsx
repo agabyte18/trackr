1ab7b717391582c35e1b644b11e9c575995391a5
Agabyte <Agabyte>
2026-01-28 18:12:40 +0100

    Remove Caching from Dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import { prisma } from "@/prisma/client";
 import IssueSummary from "./IssueSummary";
 import LatestIssues from "./LatestIssues";
 
+export const dynamic = "force-dynamic";
+
 export default async function HomePage() {
   const open = await prisma.issue.count({ where: { status: "OPEN" } });
   const wip = await prisma.issue.count({ where: { status: "IN_PROGRESS" } });
